f56ddc22bf39394933a4b8054fa4f1d8e4036316
Pasek Dawid <dawid.pasek@exorigo-upos.pl>
2019-10-30 16:55:40 +0100

    Project Startup

---
+++ src/main/java/pasek/dawid/smo/SmoApplication.java
@@ -0,0 +1,13 @@
+package pasek.dawid.smo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pasek/dawid/smo/SmoApplicationTests.java
@@ -0,0 +1,13 @@
+package pasek.dawid.smo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
